41f0a273c9f96bc64e599df4d7702369db981224
Tolga <41361745+degirmencitolga@users.noreply.github.com>
2023-02-16 11:57:48 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Website Created by Portal Grup
+          Website Created by Portal Grup v1
         </p>
         <a
           className="App-link"
